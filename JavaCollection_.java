f2556c4a33713762900f97fc6b734d27d0fa19b6
static-mkk <449016106@qq.com>
2018-03-04 15:50:25 +0800

    java集合系列01之总体框架

---
+++ JavaCollection_.java
@@ -0,0 +1,49 @@
+package cn.kkcoder.wangkuiwu;
+/**
+ * 
+ * @author static-mkk 
+ * @time 4 Mar 2018
+ * @myblog  www.kkcoder.cn
+ *  
+ *
+ */
+public class JavaCollection_ {
+
+	/**
+	 *	java集合是java提供的工具包，位于java.util.*;中.
+	 *		主要包含四个部分.
+	 *			1.List序列
+	 *			2.Set集合
+	 *			3.Map映射
+	 *			4.工具类（Iterator迭代器，枚举类，Arrays类和Collection类）
+	 *
+	 *	java集合框架图
+	 */		<img src="http://wangkuiwu.github.io/media/pic/java/collection/collection01.jpg" alt="img">
+	 /* 
+	 * 
+	 * 	由图可以看出java集合层次
+	 * 
+	 * 		1.Collection接口<AbtractCollection抽象类实现了Collection接口的方法>
+	 * 			(1)List接口<有序的队列><每个元素都有索引>
+	 * 				i.AbstractList抽象类<继承AbstractCollection抽象类><实现List接口的方法>
+	 * 					-->LinkedList	: 
+	 * 					-->ArrayList 	:
+	 * 					-->Vector    	:
+	 * 						-->Stack    : 
+	 * 			(2)Set 接口<不允许重复的无序的集合>
+	 * 				i.AbstractSet抽象类<继承AbstractCollection抽象类><实现Set接口的方法>
+	 * 					-->HashSet      :
+	 * 					-->TreeSet      :
+	 * 		2.Map接口<AbstractMap抽象类实现了Map接口方法><键值对存储信息>
+	 * 			(1)TreeMap		:
+	 * 			(2)HashMap		:
+	 * 			(3)HashTable	:
+	 *	 
+	 *		3.Iterator		：		用于遍历集合
+	 * 		4.Enumeration	：		只能在Hashtable, Vector, Stack中使用
+	 * 		5.工具类			：		Arrays类  和 Collections类 		
+	 * 
+	 */
+	
+	
+}
